8a588ef6a799fc24b6dfb741c54aa7d0a389ff6d
suzhiwei001 <37209042+suzhiwei001@users.noreply.github.com>
2018-11-22 13:08:19 +0000

    Update sss.java

--- algorithm/src/com/algorithm/sss.java
+++ algorithm/src/com/algorithm/sss.java
@@ -4,7 +4,7 @@ public class sss {
 	public static void main(String[] args) {
 		System.out.println("1");
 		System.err.println("2");
-		System.out.println("3");
+		System.out.println("36987");
 		System.out.println("4");
 		System.out.println("5");
 	}
