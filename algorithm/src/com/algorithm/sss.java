1aea2daaa06980293fc26cd48d03a3485684474c
759201272@qq.com <759201272@qq.com>
2018-11-22 21:06:39 +0800

    测试

---
+++ algorithm/src/com/algorithm/sss.java
@@ -0,0 +1,11 @@
+package com.algorithm;
+
+public class sss {
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.err.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+	}
+}
